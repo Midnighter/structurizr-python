6aa423ac00a82eb4c9c51d2a4209e9d24ccd0125
Midnighter <midnighter@posteo.net>
2020-04-06 14:56:11 +0200

    feat: add structurizr client settings

---
+++ src/structurizr/api/structurizr_client_settings.py
@@ -0,0 +1,88 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide the Structurizr client settings."""
+
+
+from getpass import getuser
+from socket import getfqdn
+
+from pydantic import UUID4, BaseSettings, Field, HttpUrl
+
+from .. import __version__
+
+
+__all__ = ("StructurizrClientSettings",)
+
+
+USER = getuser()
+hostname = getfqdn()
+if hostname:
+    USER = f"{USER}@{hostname}"
+
+AGENT = f"structurizr-python/{__version__}"
+
+
+class StructurizrClientSettings(BaseSettings):
+    """
+    Define the Structurizr client settings.
+
+    Attributes:
+        url (str): The Structurizr API URL.
+        workspace_id (int): The Structurizr workspace identifier.
+        api_key (str): The Structurizr workspace API key.
+        api_secret (str): The Structurizr workspace API secret.
+        user (str): A string identifying the user (e.g. an e-mail address or username).
+        agent (str): A string identifying the agent (e.g. 'structurizr-java/1.2.0').
+
+    """
+
+    url: HttpUrl = Field(
+        "https://api.structurizr.com",
+        env="STRUCTURIZR_URL",
+        description="The Structurizr API URL.",
+    )
+    workspace_id: int = Field(
+        ...,
+        env="STRUCTURIZR_WORKSPACE_ID",
+        description="The Structurizr workspace identifier.",
+    )
+    api_key: UUID4 = Field(
+        ...,
+        env="STRUCTURIZR_API_KEY",
+        description="The Structurizr workspace API key.",
+    )
+    api_secret: UUID4 = Field(
+        ...,
+        env="STRUCTURIZR_API_SECRET",
+        description="The Structurizr workspace API secret.",
+    )
+    user: str = Field(
+        USER,
+        env="STRUCTURIZR_USER",
+        description="A string identifying the user (e.g. an e-mail address or username).",
+    )
+    agent: str = Field(
+        AGENT,
+        env="STRUCTURIZR_AGENT",
+        description="A string identifying the agent (e.g. 'structurizr-java/1.2.0').",
+    )
+
+    class Config:
+        """Configure the Structurizr client settings."""
+
+        case_sensitive = True
+        env_prefix = "STRUCTURIZR_"
+        env_file = ".env"
---
+++ tests/test_unit/test_api/test_structurizr_client_settings.py
@@ -0,0 +1,155 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Ensure the expected behaviour of the Structurizr client settings."""
+
+
+import pytest
+from pydantic import ValidationError
+
+from structurizr.api.structurizr_client_settings import StructurizrClientSettings
+
+
+@pytest.fixture(scope="function")
+def mock_structurizr_env(monkeypatch):
+    """Reversibly modify the environment."""
+    monkeypatch.setenv("STRUCTURIZR_URL", "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+    monkeypatch.setenv("STRUCTURIZR_WORKSPACE_ID", "19")
+    monkeypatch.setenv("STRUCTURIZR_API_KEY", "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c")
+    monkeypatch.setenv("STRUCTURIZR_API_SECRET", "ae140655-da7c-4a8d-9467-5a7d9792fca0")
+    monkeypatch.setenv("STRUCTURIZR_USER", "astley@localhost")
+    monkeypatch.setenv("STRUCTURIZR_AGENT", "structurizr-python/1.0.0")
+
+
+@pytest.fixture(scope="function")
+def dotenv(tmpdir, monkeypatch):
+    """Create a `.env` file for client settings."""
+    path = tmpdir.mkdir("dotenv")
+    env_file = path.join(".env")
+    env_file.write(
+        "STRUCTURIZR_URL=https://www.youtube.com/watch?v=dQw4w9WgXcQ\n"
+        "STRUCTURIZR_WORKSPACE_ID=19\n"
+        "STRUCTURIZR_API_KEY=7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c\n"
+        "STRUCTURIZR_API_SECRET=ae140655-da7c-4a8d-9467-5a7d9792fca0\n"
+        "STRUCTURIZR_USER=astley@localhost\n"
+        "STRUCTURIZR_AGENT=structurizr-python/1.0.0\n"
+    )
+    monkeypatch.chdir(path)
+
+
+@pytest.mark.parametrize(
+    "attributes",
+    [
+        pytest.param(
+            {},
+            marks=pytest.mark.raises(
+                exception=ValidationError, message="workspace_id\n  field required"
+            ),
+        ),
+        pytest.param(
+            {},
+            marks=pytest.mark.raises(
+                exception=ValidationError, message="api_key\n  field required"
+            ),
+        ),
+        pytest.param(
+            {},
+            marks=pytest.mark.raises(
+                exception=ValidationError, message="api_secret\n  field required"
+            ),
+        ),
+        pytest.param(
+            {"workspace_id": "something"},
+            marks=pytest.mark.raises(
+                exception=ValidationError,
+                message="workspace_id\n  value is not a valid integer",
+            ),
+        ),
+        pytest.param(
+            {"api_key": "2"},
+            marks=pytest.mark.raises(
+                exception=ValidationError,
+                message="api_key\n  value is not a valid uuid",
+            ),
+        ),
+        pytest.param(
+            {"api_secret": "2"},
+            marks=pytest.mark.raises(
+                exception=ValidationError,
+                message="api_secret\n  value is not a valid uuid",
+            ),
+        ),
+        {
+            "workspace_id": 1,
+            "api_key": "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c",
+            "api_secret": "ae140655-da7c-4a8d-9467-5a7d9792fca0",
+        },
+        pytest.param(
+            {"url": "proper-url?"},
+            marks=pytest.mark.raises(
+                exception=ValidationError, message="url\n  invalid",
+            ),
+        ),
+        {
+            "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "workspace_id": 19,
+            "api_key": "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c",
+            "api_secret": "ae140655-da7c-4a8d-9467-5a7d9792fca0",
+        },
+        {
+            "workspace_id": 19,
+            "api_key": "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c",
+            "api_secret": "ae140655-da7c-4a8d-9467-5a7d9792fca0",
+            "user": "astley@localhost",
+        },
+        {
+            "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+            "workspace_id": 19,
+            "api_key": "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c",
+            "api_secret": "ae140655-da7c-4a8d-9467-5a7d9792fca0",
+            "agent": "structurizr-python/1.0.0",
+        },
+    ],
+)
+def test_init_from_arguments(attributes: dict):
+    """Expect proper initialization from arguments."""
+    settings = StructurizrClientSettings(**attributes)
+    for attr, expected in attributes.items():
+        value = getattr(settings, attr)
+        if attr in ("api_key", "api_secret"):
+            value = str(value)
+        assert value == expected
+
+
+def test_init_from_environment(mock_structurizr_env):
+    """Expect proper initialization from environment variables."""
+    settings = StructurizrClientSettings()
+    assert settings.url == "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    assert settings.workspace_id == 19
+    assert str(settings.api_key) == "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c"
+    assert str(settings.api_secret) == "ae140655-da7c-4a8d-9467-5a7d9792fca0"
+    assert settings.user == "astley@localhost"
+    assert settings.agent == "structurizr-python/1.0.0"
+
+
+def test_init_from_dotenv(dotenv):
+    """Expect proper initialization from a `.env` file."""
+    settings = StructurizrClientSettings()
+    assert settings.url == "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    assert settings.workspace_id == 19
+    assert str(settings.api_key) == "7f4e4edc-f61c-4ff2-97c9-ea4bc2a7c98c"
+    assert str(settings.api_secret) == "ae140655-da7c-4a8d-9467-5a7d9792fca0"
+    assert settings.user == "astley@localhost"
+    assert settings.agent == "structurizr-python/1.0.0"
