00f6e068fc41193754ba4b1be41ddb83b319a68b
Midnighter <midnighter@posteo.net>
2020-04-10 18:05:25 +0200

    feat: add a minimal model item class

---
+++ src/structurizr/model/model_item.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide the base class for elements and relationships."""
+
+
+from typing import Dict, Set
+
+from pydantic import BaseModel, Field
+
+from .perspective import Perspective
+
+
+__all__ = ("ModelItem",)
+
+
+class ModelItem(BaseModel):
+    """
+    Define a base class for elements and relationships.
+
+    Attributes:
+        id (str):
+        origin_id (str):
+        tags (set of str):
+        properties (dict):
+        perspectives (set of Perspective):
+
+    """
+
+    id: str = Field("")
+    origin_id: str = Field("", alias="originId")
+    tags: Set[str] = Field(set())
+    properties: Dict[str, str] = Field({})
+    perspectives: Set[Perspective] = Field(set())
---
+++ tests/test_unit/test_model/test_model_item.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Ensure the expected behaviour of the model item base class."""
+
+
+import pytest
+
+from structurizr.model.model_item import ModelItem
+
+
+@pytest.mark.parametrize(
+    "attributes", [{},],
+)
+def test_model_item_init(attributes):
+    """Expect proper initialization from arguments."""
+    model_item = ModelItem(**attributes)
+    for attr, expected in attributes.items():
+        assert getattr(model_item, attr) == expected
