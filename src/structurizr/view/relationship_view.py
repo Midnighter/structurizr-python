8d7db73052cf2809f56508501c9abce7b3abbf2b
Midnighter <midnighter@posteo.net>
2020-04-15 02:09:38 +0200

    feat: add basic relationship view

---
+++ src/structurizr/view/relationship_view.py
@@ -0,0 +1,48 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Provide a container for a relationship instance in a view."""
+
+
+from typing import Any, Optional, Set
+
+from ..base import Base
+from ..model import Relationship
+
+
+__all__ = ("RelationshipView",)
+
+
+START_OF_LINE = 0
+END_OF_LINE = 100
+
+
+class RelationshipView(Base):
+    """
+    Represent an instance of a relationship in a view.
+
+    Attributes:
+
+    """
+
+    relationship: Relationship
+    id: Optional[str]
+    description: Optional[str]
+    order: Optional[str]
+    # TODO
+    vertices: Set[Any] = set()
+    # TODO
+    routing: Optional[Any]
+    position: Optional[int]
