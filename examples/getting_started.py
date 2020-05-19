a0b2727f510aadef622f87ca449f75442ad4b5d6
Midnighter <midnighter@posteo.net>
2020-05-19 12:21:40 +0200

    tests: create workspace IO integration tests

---
+++ examples/getting_started.py
@@ -0,0 +1,63 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""
+Provide a 'getting started' example.
+
+Illustrate how to create a software architecture diagram using code.
+"""
+
+
+import logging
+
+from structurizr import Workspace
+from structurizr.model import Tags
+from structurizr.view import ElementStyle
+
+
+def main() -> Workspace:
+    """Create the 'getting started' example."""
+    workspace = Workspace(
+        name="Getting Started",
+        description="This is a model of my software system which can be found at "
+        "https://structurizr.com/help/examples.",
+    )
+
+    model = workspace.model
+
+    user = model.add_person(name="User", description="A user of my software system.")
+    software_system = model.add_software_system(
+        name="Software System", description="My software system."
+    )
+    user.uses(software_system, "Uses")
+
+    context_view = workspace.views.create_system_context_view(
+        software_system=software_system,
+        key="SystemContext",
+        description="An example of a System Context diagram.",
+    )
+    context_view.add_all_elements()
+
+    styles = workspace.views.configuration.styles
+    styles.add(
+        ElementStyle(tag=Tags.SOFTWARE_SYSTEM, background="#1168bd", color="#ffffff")
+    )
+    styles.add(ElementStyle(tag=Tags.PERSON, background="#08427b", color="#ffffff"))
+    return workspace
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level="INFO")
+    main()
---
+++ tests/test_integration/test_workspace_io.py
@@ -0,0 +1,67 @@
+# Copyright (c) 2020, Moritz E. Beber.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Ensure correct workspace (de-)serialization."""
+
+
+from importlib import import_module
+from pathlib import Path
+
+import pytest
+from pydantic import ValidationError
+
+from structurizr import Workspace, WorkspaceIO
+
+
+DEFINITIONS = Path("data") / "workspace_definition"
+EXAMPLES = Path(__file__).parent.parent.parent / "examples"
+VALIDATIONS = Path("data") / "workspace_validation"
+
+
+def pytest_generate_tests(metafunc) -> None:
+    if "invalid_workspace" in metafunc.fixturenames:
+        files = sorted(Path("data", "workspace_validation").glob("*.json.gz"))
+        ids = [p.name for p in files]
+        metafunc.parametrize(
+            "invalid_workspace",
+            [
+                pytest.param(f, marks=pytest.mark.raises(exception=ValidationError))
+                for f in files
+            ],
+            ids=ids,
+        )
+
+
+def test_invalid_workspace(invalid_workspace):
+    WorkspaceIO.parse_file(invalid_workspace)
+
+
+@pytest.mark.parametrize("filename", ["Trivial.json.gz", "GettingStarted.json.gz"])
+def test_deserialize_workspace(filename):
+    """Expect that a trivial workspace definition is successfully deserialized."""
+    path = DEFINITIONS / filename
+    Workspace.load(path)
+
+
+@pytest.mark.parametrize(
+    "example, filename", [("getting_started", "GettingStarted.json.gz")]
+)
+def test_serialize_workspace(example, filename, monkeypatch):
+    """Expect that ."""
+    monkeypatch.syspath_prepend(EXAMPLES)
+    example = import_module(example)
+    path = DEFINITIONS / filename
+    expected = Workspace.load(path)
+    assert example.main() == expected
