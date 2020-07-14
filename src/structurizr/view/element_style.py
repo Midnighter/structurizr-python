a6c9ae5167495d7ac05106f9a31044f80c354bfa
ilaif <ifallach@gmail.com>
2020-07-14 15:25:10 +0300

    [ADD] contributions

--- src/structurizr/view/element_style.py
+++ src/structurizr/view/element_style.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Moritz E. Beber.
+# Copyright (c) 2020, Moritz E. Beber, Ilai Fallach.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
