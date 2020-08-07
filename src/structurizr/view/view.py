0d01c1c7165bae406901db8545fbf10ce0dea974
ilaif <ifallach@gmail.com>
2020-08-07 22:41:09 +0200

    feat: build out view features
    Co-authored-by: Midnighter <midnighter@posteo.net>

--- src/structurizr/view/view.py
+++ src/structurizr/view/view.py
@@ -24,7 +24,7 @@ from pydantic import Field
 from ..abstract_base import AbstractBase
 from ..base_model import BaseModel
 from ..mixin import ViewSetRefMixin
-from ..model import Element, SoftwareSystem, SoftwareSystemIO
+from ..model import Element, SoftwareSystem
 from .automatic_layout import AutomaticLayout, AutomaticLayoutIO
 from .element_view import ElementView, ElementViewIO
 from .paper_size import PaperSize
@@ -46,17 +46,22 @@ class ViewIO(BaseModel, ABC):
 
     key: str
     description: str
-    software_system: Optional[SoftwareSystemIO] = Field(None, alias="softwareSystem")
-    software_system_id: str = Field("", alias="softwareSystemId")
-    paper_size: Optional[PaperSize] = Field(None, alias="paperSize")
-    automatic_layout: Optional[AutomaticLayoutIO] = Field(None, alias="automaticLayout")
+    software_system_id: str = Field(default="", alias="softwareSystemId")
+    paper_size: Optional[PaperSize] = Field(default=None, alias="paperSize")
+    automatic_layout: Optional[AutomaticLayoutIO] = Field(
+        default=None, alias="automaticLayout"
+    )
     title: str = ""
 
-    element_views: List[ElementViewIO] = Field([], alias="elementViews")
-    relationship_views: List[RelationshipViewIO] = Field([], alias="relationshipViews")
+    element_views: List[ElementViewIO] = Field(default=(), alias="elementViews")
+    relationship_views: List[RelationshipViewIO] = Field(
+        default=(), alias="relationshipViews"
+    )
 
     # TODO
-    layout_merge_strategy: Optional[Any] = Field(None, alias="layoutMergeStrategy")
+    layout_merge_strategy: Optional[Any] = Field(
+        default=None, alias="layoutMergeStrategy"
+    )
 
 
 class View(ViewSetRefMixin, AbstractBase, ABC):
@@ -72,31 +77,35 @@ class View(ViewSetRefMixin, AbstractBase, ABC):
     def __init__(
         self,
         *,
-        software_system: SoftwareSystem,
-        key: str,
+        software_system: Optional[SoftwareSystem] = None,
+        key: str = None,
         description: str,
         paper_size: Optional[PaperSize] = None,
         automatic_layout: Optional[AutomaticLayout] = None,
         title: str = "",
-        element_views: Optional[Iterable[ElementView]] = None,
-        relationship_views: Optional[Iterable[RelationshipView]] = None,
+        element_views: Optional[Iterable[ElementView]] = (),
+        relationship_views: Optional[Iterable[RelationshipView]] = (),
         layout_merge_strategy: Optional[Any] = None,
         **kwargs,
     ):
         """Initialize a view with a 'private' view set."""
         super().__init__(**kwargs)
         self.software_system = software_system
+        self.software_system_id = software_system.id if software_system else None
         self.key = key
         self.description = description
         self.paper_size = paper_size
         self.automatic_layout = automatic_layout
         self.title = title
-        self.element_views = set() if element_views is None else set(element_views)
-        self.relationship_views = (
-            set() if relationship_views is None else set(relationship_views)
-        )
+        self.element_views = set(element_views)
+        self.relationship_views = set(relationship_views)
+
         # TODO
         self.layout_merge_strategy = layout_merge_strategy
+
+    @property
+    def model(self):
+        return self.software_system.get_model()
 
     def _add_element(self, element: Element, add_relationships: bool) -> None:
         """
@@ -108,12 +117,13 @@ class View(ViewSetRefMixin, AbstractBase, ABC):
                 relationships with other elements.
 
         """
-        if element not in self.software_system.get_model():
+        if element not in self.model:
             raise RuntimeError(
                 f"The element {element} does not exist in the model associated with "
                 f"this view."
             )
-        self.element_views.add(ElementView(element=element))
+        # TODO: finish x,y coordinates , x=0, y=0
+        self.element_views.add(ElementView(id=element.id))
         if add_relationships:
             self._add_relationships(element)
 
@@ -125,10 +135,14 @@ class View(ViewSetRefMixin, AbstractBase, ABC):
             element (Element): The model element.
 
         """
-        elements: Set[Element] = {v.element for v in self.element_views}
+        elements: Set[str] = {v.id for v in self.element_views}
+
         for relationship in element.get_efferent_relationships():
-            if relationship.destination in elements:
-                self.relationship_views.add(RelationshipView(relationship=relationship))
+            if relationship.destination.id in elements:
+                # TODO: finish relationshipview construction
+                self.relationship_views.add(RelationshipView(id=relationship.id))
+
         for relationship in element.get_afferent_relationships():
-            if relationship.source in elements:
-                self.relationship_views.add(RelationshipView(relationship=relationship))
+            if relationship.source.id in elements:
+                # TODO: finish relationshipview construction
+                self.relationship_views.add(RelationshipView(id=relationship.id))
