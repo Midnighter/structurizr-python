3c1e5c9f62347a25006f041b88863df22bd350f9
ilaif <ifallach@gmail.com>
2020-08-07 23:16:31 +0200

    feat: add hydrate and other features to view set
    Co-authored-by: Midnighter <midnighter@posteo.net>

--- src/structurizr/view/view_set.py
+++ src/structurizr/view/view_set.py
@@ -24,7 +24,9 @@ from ..abstract_base import AbstractBase
 from ..base_model import BaseModel
 from ..mixin import ModelRefMixin
 from .configuration import Configuration, ConfigurationIO
+from .container_view import ContainerView, ContainerViewIO
 from .system_context_view import SystemContextView, SystemContextViewIO
+from .system_landscape_view import SystemLandscapeView, SystemLandscapeViewIO
 
 
 if TYPE_CHECKING:
@@ -44,20 +46,20 @@ class ViewSetIO(BaseModel):
 
     """
 
-    # TODO
-    # system_landscape_views: Set[SystemLandscapeView] = Field(
-    #     set(), alias="systemLandscapeViews"
-    # )
-    system_context_views: List[SystemContextViewIO] = Field(
-        [], alias="systemContextView"
+    system_landscape_views: List[SystemLandscapeViewIO] = Field(
+        default=(), alias="systemLandscapeViews"
     )
-    configuration: Optional[ConfigurationIO]
-    # TODO
-    # container_views: Set[ContainerView] = Field(set(), alias="containerViews")
-    # component_views: Set[ComponentView] = Field(set(), alias="componentViews")
-    # dynamic_views: Set[DynamicView] = Field(set(), alias="dynamicViews")
-    # deployment_views: Set[DeploymentView] = Field(set(), alias="deploymentViews")
-    # filtered_views: Set[FilteredView] = Field(set(), alias="filteredViews")
+    system_context_views: List[SystemContextViewIO] = Field(
+        default=(), alias="systemContextViews"
+    )
+    configuration: Optional[ConfigurationIO] = None
+    container_views: List[ContainerViewIO] = Field(default=(), alias="containerViews")
+
+    # TODO:
+    # component_views: List[ComponentView] = Field(set(), alias="componentViews")
+    # dynamic_views: List[DynamicView] = Field(set(), alias="dynamicViews")
+    # deployment_views: List[DeploymentView] = Field(set(), alias="deploymentViews")
+    # filtered_views: List[FilteredView] = Field(set(), alias="filteredViews")
 
 
 class ViewSet(ModelRefMixin, AbstractBase):
@@ -73,21 +75,42 @@ class ViewSet(ModelRefMixin, AbstractBase):
     def __init__(
         self,
         *,
-        model: "Model",
+        model: Optional["Model"] = None,
+        # TODO:
+        # enterprise_context_views: Iterable[EnterpriseContextView] = (),
+        system_landscape_views: Iterable[SystemLandscapeView] = (),
         system_context_views: Iterable[SystemContextView] = (),
+        container_views: Iterable[ContainerView] = (),
         configuration: Optional[Configuration] = None,
         **kwargs
     ) -> None:
         """Initialize a view set."""
         super().__init__(**kwargs)
+        # self.enterprise_context_views = set(enterprise_context_views)
+        self.system_landscape_views = set(system_landscape_views)
         self.system_context_views = set(system_context_views)
+        self.container_views = set(container_views)
         # TODO
         self.configuration = Configuration() if configuration is None else configuration
         self.set_model(model)
 
+    def create_system_landscape_view(
+        self, system_landscape_view: Optional[SystemLandscapeView] = None, **kwargs
+    ) -> SystemLandscapeView:
+        # TODO:
+        # assertThatTheViewKeyIsSpecifiedAndUnique(key);
+        if system_landscape_view is None:
+            system_landscape_view = SystemLandscapeView(
+                model=self.get_model(), **kwargs
+            )
+        system_landscape_view.set_viewset(self)
+        self.system_landscape_views.add(system_landscape_view)
+        return system_landscape_view
+
     def create_system_context_view(
-        self, system_context_view: SystemContextView = None, **kwargs
+        self, system_context_view: Optional[SystemContextView] = None, **kwargs
     ) -> SystemContextView:
+        # TODO:
         # assertThatTheSoftwareSystemIsNotNull(softwareSystem);
         # assertThatTheViewKeyIsSpecifiedAndUnique(key);
         if system_context_view is None:
@@ -95,3 +118,38 @@ class ViewSet(ModelRefMixin, AbstractBase):
         system_context_view.set_viewset(self)
         self.system_context_views.add(system_context_view)
         return system_context_view
+
+    def create_container_view(
+        self, container_view: Optional[ContainerView] = None, **kwargs
+    ) -> ContainerView:
+        # TODO:
+        # assertThatTheSoftwareSystemIsNotNull(softwareSystem);
+        # assertThatTheViewKeyIsSpecifiedAndUnique(key);
+        if container_view is None:
+            container_view = ContainerView(**kwargs)
+        container_view.set_viewset(self)
+        self.container_views.add(container_view)
+        return container_view
+
+    @classmethod
+    def hydrate(cls, views: ViewSetIO, model: Optional["Model"] = None) -> "ViewSet":
+        system_context_views = []
+        for view_io in views.system_context_views:
+            view = SystemContextView.hydrate(view_io)
+            view.software_system_id = view_io.software_system_id
+            view.software_system = model.get_software_system_with_id(
+                view.software_system_id
+            )
+            system_context_views.append(view)
+
+        return cls(
+            # model=model,
+            # TODO:
+            # enterprise_context_views: Iterable[EnterpriseContextView] = (),
+            system_landscape_views=map(
+                SystemLandscapeView.hydrate, views.system_landscape_views
+            ),
+            system_context_views=system_context_views,
+            container_views=map(ContainerView.hydrate, views.container_views),
+            configuration=Configuration.hydrate(views.configuration),
+        )
