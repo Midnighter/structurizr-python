6eaa6af3fd7eade20dfa2ab1527189c8f15722e2
yt-ms <71541861+yt-ms@users.noreply.github.com>
2021-05-10 10:56:11 +0100

    feat: add relationships to DynamicView

--- src/structurizr/view/dynamic_view.py
+++ src/structurizr/view/dynamic_view.py
@@ -20,13 +20,13 @@ free-form arrangement of diagram elements with numbered interactions to indicate
 ordering.
 """
 
-from typing import Optional, Union
+from typing import Optional, Tuple, Union
 
 from pydantic import Field
 
 from ..mixin.model_ref_mixin import ModelRefMixin
-from ..model.container import Container
-from ..model.software_system import SoftwareSystem
+from ..model import Container, Element, Relationship, SoftwareSystem
+from .relationship_view import RelationshipView
 from .view import View, ViewIO
 
 
@@ -57,7 +57,7 @@ class DynamicView(ModelRefMixin, View):
         *,
         software_system: Optional[SoftwareSystem] = None,
         container: Optional[Container] = None,
-        **kwargs
+        **kwargs,
     ) -> None:
         """Initialize a DynamicView.
 
@@ -70,6 +70,80 @@ class DynamicView(ModelRefMixin, View):
         super().__init__(**kwargs)
         self.element = software_system or container
         self.element_id = self.element.id if self.element else None
+
+    def add(
+        self,
+        source: Element,
+        description: str,
+        destination: Element,
+        *,
+        technology: Optional[str] = None,
+    ) -> RelationshipView:
+        """Add a relationship to this DynamicView.
+
+        This will search for a relationship in the model from the source to the
+        destination with matching description and technology (if specified).  It
+        will also look for situations where this interaction is a "response" in
+        that it it goes in the opposite direction to the relationship in the
+        model, and in this case then description is ignored for matching but will
+        appear in the view.
+
+        Example:
+            dynamic_view.add(container1, "Requests data from", container2)
+            dynamic_view.add(container2, "Sends response back to" container1)
+        """
+        relationship, response = self._find_relationship(
+            source, description, destination, technology
+        )
+        if relationship is None:
+            if technology:
+                raise ValueError(
+                    f"A relationship between {source.name} and "
+                    f"{destination.name} with technology "
+                    f"'{technology}' does not exist in model."
+                )
+            else:
+                raise ValueError(
+                    f"A relationship between {source.name} and "
+                    f"{destination.name} does not exist in "
+                    "model."
+                )
+        self._add_element(source, False)
+        self._add_element(destination, False)
+        return self._add_relationship(
+            relationship, description=description, response=response
+        )
+
+    def _find_relationship(
+        self,
+        source: Element,
+        description: str,
+        destination: Element,
+        technology: Optional[str],
+    ) -> Tuple[Optional[Relationship], bool]:
+        rel = next(
+            (
+                rel
+                for rel in source.get_efferent_relationships()
+                if rel.description == description
+                and (technology is None or rel.technology == technology)
+            ),
+            None,
+        )
+        if rel:
+            return rel, False
+
+        # Look for "response" to relationship in the opposite direction but ignore
+        # descriptions
+        rel = next(
+            (
+                rel
+                for rel in source.get_afferent_relationships()
+                if technology is None or rel.technology == technology
+            ),
+            None,
+        )
+        return rel, True
 
     @classmethod
     def hydrate(
--- tests/unit/view/test_dynamic_view.py
+++ tests/unit/view/test_dynamic_view.py
@@ -14,27 +14,36 @@
 
 import pytest
 
-from structurizr import Workspace
-from structurizr.model import Container, SoftwareSystem
+from structurizr.model import Container, Model, SoftwareSystem
 from structurizr.view.dynamic_view import DynamicView, DynamicViewIO
 
 
 @pytest.fixture(scope="function")
-def empty_workspace() -> Workspace:
-    """Provide an empty Workspace on demand for test cases to use."""
-    return Workspace(name="Name", description="Description")
+def empty_model() -> Model:
+    """Provide an empty Model on demand for test cases to use."""
+    yield Model()
 
 
-def test_create_new_dynamic_view(empty_workspace: Workspace):
+@pytest.fixture(scope="function")
+def empty_view(empty_model) -> DynamicView:
+    """Provide an empty DynamicView on demand for test cases to use."""
+    view = DynamicView(key="dyn1", description="Dynamic view")
+    view.set_model(empty_model)
+    yield view
+
+
+def test_create_new_dynamic_view(empty_model: Model):
     """Test basic construction."""
     view = DynamicView(description="Test view")
     assert view.description == "Test view"
+    view.set_model(empty_model)
+    assert view.model is empty_model
 
 
-def test_cosntructor_param_validation(empty_workspace: Workspace):
+def test_constructor_param_validation():
     """Test validation of constructor parameters."""
     system = SoftwareSystem(name="sys1")
-    container = Container(name="con1")
+    container = Container(name="con1", parent=system)
 
     view1 = DynamicView(description="Description")
     assert view1.element is None
@@ -48,12 +57,95 @@ def test_cosntructor_param_validation(empty_workspace: Workspace):
         )
 
 
-def test_hydration(empty_workspace: Workspace):
+def test_adding_relationships_finds_correct_relationship(empty_view: DynamicView):
+    """Check logic for matching which relationship to use when adding."""
+    model = empty_view.model
+    system1 = model.add_software_system(name="System 1", id="sys1")
+    system2 = model.add_software_system(name="System 2", id="sys2")
+    system3 = model.add_software_system(name="System 3", id="sys3")
+    rel1 = model.add_relationship(
+        source=system1, destination=system2, description="Sends requests to"
+    )
+    rel2 = model.add_relationship(
+        source=system2, destination=system3, description="Invokes", technology="REST"
+    )
+    rel3 = model.add_relationship(
+        source=system2, destination=system3, description="Invokes", technology="SOAP"
+    )
+
+    assert empty_view.add(system1, "Sends requests to", system2).relationship is rel1
+    assert not empty_view.add(system1, "Sends requests to", system2).response
+    assert (
+        empty_view.add(system2, "Invokes", system3, technology="REST").relationship
+        is rel2
+    )
+    assert (
+        empty_view.add(system2, "Invokes", system3, technology="SOAP").relationship
+        is rel3
+    )
+
+
+def test_matching_on_response_relationship(empty_view: DynamicView):
+    """Check it correctly finds relationships for responses."""
+    model = empty_view.model
+    system1 = model.add_software_system(name="System 1", id="sys1")
+    system2 = model.add_software_system(name="System 2", id="sys2")
+    system3 = model.add_software_system(name="System 3", id="sys3")
+    rel1 = model.add_relationship(
+        source=system1, destination=system2, description="Sends requests to"
+    )
+    model.add_relationship(
+        source=system2, destination=system3, description="Invokes", technology="REST"
+    )
+    rel3 = model.add_relationship(
+        source=system2, destination=system3, description="Invokes", technology="SOAP"
+    )
+
+    view = empty_view.add(system2, "Sends response back to", system1)
+    assert view.relationship is rel1
+    assert view.response
+    assert view.description == "Sends response back to"
+
+    view = empty_view.add(system3, "Replies back to", system2, technology="SOAP")
+    assert view.relationship is rel3
+    assert view.response
+    assert view.description == "Replies back to"
+
+
+def test_adding_relationships_failure_cases(empty_view: DynamicView):
+    """Test common failure cases for adding relationships."""
+    model = empty_view.model
+    system1 = model.add_software_system(name="System 1", id="sys1")
+    system2 = model.add_software_system(name="System 2", id="sys2")
+    model.add_relationship(
+        source=system1,
+        destination=system2,
+        description="Sends requests to",
+        technology="REST",
+    )
+
+    with pytest.raises(
+        ValueError, match="A relationship between System 1 and System 2"
+    ):
+        empty_view.add(system1, "Bogus description", system2)
+    with pytest.raises(ValueError, match="with technology 'Bogus'"):
+        empty_view.add(system1, "Sends requests to", system2, technology="Bogus")
+    with pytest.raises(ValueError, match="with technology 'Bogus'"):
+        empty_view.add(system2, "Sends response back to", system1, technology="Bogus")
+
+
+@pytest.mark.xfail(sctrict=True)
+def test_trying_to_add_element_outside_scope(empty_view: DynamicView):
+    """Ensure adding relationships beyond this scope fails."""
+    assert 1 == 0  # TODO
+
+
+def test_hydration(empty_model: Model):
     """Check dehydrating and hydrating."""
-    system = empty_workspace.model.add_software_system(name="system", id="sys1")
+    system = empty_model.add_software_system(name="system", id="sys1")
 
     view = DynamicView(key="dyn1", description="Description", software_system=system)
-    view.set_model(empty_workspace.model)
+    view.set_model(empty_model)
 
     io = DynamicViewIO.from_orm(view)
     d = io.dict()
