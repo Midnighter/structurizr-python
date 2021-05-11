e57f4f12eba6b4e7935e4c4d14795a6afb37580c
yt-ms <71541861+yt-ms@users.noreply.github.com>
2021-05-11 11:49:03 +0100

    test: ensure layout information is copied over for dynamic views

--- tests/unit/view/test_view_set.py
+++ tests/unit/view/test_view_set.py
@@ -15,13 +15,20 @@
 import pytest
 
 from structurizr.model.model import Model
+from structurizr.view.paper_size import PaperSize
 from structurizr.view.view_set import ViewSet, ViewSetIO
 
 
 @pytest.fixture(scope="function")
 def empty_model() -> Model:
     """Provide an empty model for testing."""
-    return Model()
+    yield Model()
+
+
+@pytest.fixture(scope="function")
+def empty_viewset(empty_model) -> ViewSet:
+    """Provide an empty viewset for testing."""
+    yield ViewSet(model=empty_model)
 
 
 def test_view_set_construction(empty_model):
@@ -41,23 +48,41 @@ def test_adding_dynamic_view(empty_model):
     assert view in viewset.dynamic_views
 
 
-def test_dynamic_view_hydrated(empty_model):
+def test_dynamic_view_hydrated(empty_viewset):
     """Check dynamic views hydrated properly."""
-    viewset = ViewSet(model=empty_model)
-    system1 = empty_model.add_software_system(name="sys1")
+    viewset = empty_viewset
+    system1 = viewset.model.add_software_system(name="sys1")
     viewset.create_dynamic_view(
         key="dyn1", description="dynamic", software_system=system1
     )
     io = ViewSetIO.from_orm(viewset)
 
-    new_viewset = ViewSet.hydrate(io, empty_model)
+    new_viewset = ViewSet.hydrate(io, viewset.model)
     assert len(new_viewset.dynamic_views) == 1
     view = list(new_viewset.dynamic_views)[0]
     assert view.description == "dynamic"
     assert view.element is system1
 
 
+def test_copying_dynamic_view_layout(empty_viewset):
+    """Check that layout info is copied over for Dynamic views."""
+    source_viewset = empty_viewset
+    source_view = source_viewset.create_dynamic_view(key="dyn1", description="test")
+    source_view.paper_size = PaperSize.A4_Landscape
+
+    target_viewset = ViewSet(model=empty_viewset.model)
+
+    # Check it's OK if there isn't a matching view in the target
+    target_viewset.copy_layout_information_from(source_viewset)
+
+    # Now try one where we have a match
+    target_view = target_viewset.create_dynamic_view(key="dyn1", description="test2")
+    assert target_view.paper_size is None
+    target_viewset.copy_layout_information_from(source_viewset)
+    assert target_view.paper_size == PaperSize.A4_Landscape
+
+
 @pytest.mark.xfail(strict=True)
 def test_copying_layout(empty_model):
-    """Check copying layout from another viewset."""
+    """Check copying layout from other view types."""
     assert 1 == 0  # TODO
